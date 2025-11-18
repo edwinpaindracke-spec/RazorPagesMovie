5c028abc2f2eb1a0fbd98ab5e4ca6a5684857175
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-11-18 15:02:16 +0200

    fixing errors cuz it wont be able to push to git hub

--- RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2346ae87c37351009b88531926733d3725d893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5eebb8351ed182305c62f3a475f2641864a433")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
