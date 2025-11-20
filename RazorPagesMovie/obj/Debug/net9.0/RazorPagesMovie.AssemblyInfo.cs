51c23587f31ee6e178f1480e217ff972a641cdee
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-11-20 11:40:15 +0200

    Part 6, add search to ASP.NET Core Razor Pages

--- RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c028abc2f2eb1a0fbd98ab5e4ca6a5684857175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d45af788888da1d14e0021a5bd70a56dd72816")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
