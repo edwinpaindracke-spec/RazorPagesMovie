bd93e4368f9b5f3cdfc3834ff84f8da696ca6f48
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-11-24 12:37:26 +0200

    adding a functionality of home page that has a movie button link in the home page

--- RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b060cdda37018c4440f6aebc29934725bd4ff40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4613108e53fcaa22dd513503f3a22bcea558ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
