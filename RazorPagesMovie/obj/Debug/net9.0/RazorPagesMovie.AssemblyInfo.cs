55f848dcb7f4f84b77a35c7d4b6965c3c3a9f6f1
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-11-26 11:34:27 +0200

    added a picture for the create movie

--- RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec8d606b88082a5d90109d887f6e489311ccccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18b84b1b40fb2ef63d7a867d40770a192bd08744")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
